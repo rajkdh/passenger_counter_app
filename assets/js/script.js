3bd5e60793531f494c011f5948c6bce12cf622af
rajkdh <112664790+rajkdh@users.noreply.github.com>
2022-09-26 21:44:26 -0400

    initial commit - updating javascirpt and html file

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//document.getElementById("count").innerText = 5
+
+let countEl = document.getElementById('count-el')
+
+let totalCount = 0;
+
+function increment() {
+    totalCount += 1
+    countEl.innerText = totalCount
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    //console.log(totalCount);
+}
+
+
+
+
+
+
+
+
+
+
+
+//           RANDOM
+
